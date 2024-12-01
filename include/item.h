acc90412751f83da9215a2de98a2abd5e07a6dc0
Luka <gruda5959@gmail.com>
2024-12-01 19:44:51 +0100

    Setup item.h

---
+++ include/item.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <string>
+#define DEFAULT_MIN_QUANTITY 0
+#define DEFAULT_QUANTITY 0
+#define DEFAULT_UNIT "unit"
+#define DEFAULT_NAME "item"
+
+class Item
+{
+public:
+    // konstruktor
+    Item(std::string name = DEFAULT_NAME, std::string unit = DEFAULT_UNIT, int quantity = DEFAULT_QUANTITY, int minQuantity = DEFAULT_MIN_QUANTITY);
+
+    // destruktor
+    ~Item();
+
+    // geteri
+    std::string getName();
+    std::string getUnit();
+    int getQuantity();
+    int getMinQuantity();
+
+    // seteri
+    void setName(std::string name);
+    void setUnit(std::string unit);
+    void setQuantity(int quantity);
+    void setMinQuantity(int minQuantity);
+
+    // info
+    void info();
+
+private:
+    std::string name;
+    std::string unit;
+    int quantity;
+    int minQuantity;
+};
